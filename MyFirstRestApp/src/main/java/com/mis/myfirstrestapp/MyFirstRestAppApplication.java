5bd91e5e90830d1561393b8f17511dbf74141809
Garvit Shubham <mailme.garvit@gmail.com>
2023-03-14 15:48:07 +0530

    My first Rest app done

--- MyFirstRestApp/src/main/java/com/mis/myfirstrestapp/MyFirstRestAppApplication.java
+++ MyFirstRestApp/src/main/java/com/mis/myfirstrestapp/MyFirstRestAppApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.*")
+@ComponentScan(basePackages = {"com.*"})
 public class MyFirstRestAppApplication {
 
     public static void main(String[] args) {
