e9ed4db4580f70e347d3f4ca02e2e70ab8fa82a5
Garvit Shubham <mailme.garvit@gmail.com>
2023-03-15 13:33:31 +0530

    Spring data Jpa project: Creating a table

---
+++ SpringRestJPADemo/src/main/java/com/mis/jpa/model/Employee.java
@@ -0,0 +1,64 @@
+package com.mis.jpa.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="employee")
+public class Employee {
+    @Id
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "salary")
+    private Double salary;
+
+    public Employee(long id, String name, String email, Double salary) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.salary = salary;
+    }
+
+    public Employee(){
+
+    }
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+}
