e71de9376d053f848f9d87475337bf15dc316c20
Garvit Shubham <mailme.garvit@gmail.com>
2023-03-17 17:13:48 +0530

    Microservice internal communications with Exception Handling

--- MicroservicesDemo/user-service/src/main/java/com/controller/UserController.java
+++ MicroservicesDemo/user-service/src/main/java/com/controller/UserController.java
@@ -3,6 +3,7 @@ package com.controller;
 import com.VO.Department;
 import com.VO.ResponseTemplateVO;
 import com.entity.Users;
+import com.exception.DepartmentIdDoesNotExistException;
 import com.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -33,12 +34,16 @@ public class UserController {
 
 
     @GetMapping("/getdepname/{email}")
-    public String getDepName(@PathVariable("email") String email){
-        return service.getDepartmentName(email);
+    public String getDepName(@PathVariable("email") String email) throws DepartmentIdDoesNotExistException{
+        try {
+            return service.getDepartmentName(email);
+        } catch(Exception e){
+            throw new DepartmentIdDoesNotExistException("Department ID for the individual does not seem correct");
+        }
     }
 
     @PutMapping("/modifydep/{id}/{name}")
-    public Department modifyDepartment(@PathVariable("id") Long id, @PathVariable("name") String name) {
+    public Department modifyDepartment(@PathVariable("id") Long id, @PathVariable("name") String name) throws DepartmentIdDoesNotExistException {
         return service.modifyDepartment(id,name);
     }
 }
--- MicroservicesDemo/user-service/src/main/java/com/service/UserService.java
+++ MicroservicesDemo/user-service/src/main/java/com/service/UserService.java
@@ -3,6 +3,7 @@ package com.service;
 import com.VO.Department;
 import com.VO.ResponseTemplateVO;
 import com.entity.Users;
+import com.exception.DepartmentIdDoesNotExistException;
 import com.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,37 +24,48 @@ public class UserService {
     @Autowired
     RestTemplate restTemplate;
 
+    private static final String URL = "http://department-service/departments/getbyid/";
+
     public ResponseTemplateVO getUserByDepartment(Long userid){
         ResponseTemplateVO obj = new ResponseTemplateVO();
         Users user = userRepository.findByUserId(userid);
         System.out.println("hi"+user);
-        Department department = restTemplate.getForObject("http://department-service/departments/getbyid/"+user.getDepartmentId(), Department.class);
+        Department department = restTemplate.getForObject(URL+user.getDepartmentId(), Department.class);
         System.out.println("hello"+department);
         obj.setUser(user);
         obj.setDepartment(department);
         return obj;
     }
 
-    public String getDepartmentName(String email){
+    public String getDepartmentName(String email) throws DepartmentIdDoesNotExistException{
         Users user = userRepository.findByEmailId(email);
         System.out.println(user);
-        Department department = restTemplate.getForObject("http://department-service/departments/getbyid/"+user.getDepartmentId(), Department.class);
-        System.out.println(department);
-        return department.getDepartmentName();
+        Department department = restTemplate.getForObject(URL+user.getDepartmentId(), Department.class);
+        if(department!=null) {
+            System.out.println(department);
+            return department.getDepartmentName();
+        }
+        else {
+            throw new DepartmentIdDoesNotExistException("Department ID for the individual does not seem correct");
+        }
     }
 
 
     //First doing it without exceptions
     //Then try with taking care of exceptions
-    public Department modifyDepartment(Long id, String depName){
+    public Department modifyDepartment(Long id, String depName) throws DepartmentIdDoesNotExistException {
         Users user = userRepository.findByUserId(id);
         System.out.println(user);
-        Department department = restTemplate.getForObject("http://department-service/departments/getbyid/"+user.getDepartmentId(),Department.class);
-        System.out.println(department);
-        assert department != null;
-        department.setDepartmentName(depName);
-        restTemplate.put("http://department-service/departments/update/"+department.getDepartmentId()+"/" + department.getDepartmentName(),Department.class);
-        return department;
+        Department department = restTemplate.getForObject(URL+user.getDepartmentId(),Department.class);
+        if(department!=null) {
+            System.out.println(department);
+            department.setDepartmentName(depName);
+            restTemplate.put("http://department-service/departments/update/" + department.getDepartmentId() + "/" + department.getDepartmentName(), Department.class);
+            return department;
+        }
+        else {
+            throw new DepartmentIdDoesNotExistException("Department ID for the individual does not seem correct");
+        }
     }
 
     public List<Users> getDepartmentUsers(Long departmentId){
