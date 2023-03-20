55fbc03784413d615cebc2c8290cabdad4762c43
Garvit Shubham <mailme.garvit@gmail.com>
2023-03-20 18:38:16 +0530

    Initialising Bus Bookings app

---
+++ BusBookingApps/src/main/java/com/mis/busbookingapps/BusBookingAppsApplication.java
@@ -0,0 +1,13 @@
+package com.mis.busbookingapps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusBookingAppsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusBookingAppsApplication.class, args);
+    }
+
+}
---
+++ BusBookingApps/src/test/java/com/mis/busbookingapps/BusBookingAppsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mis.busbookingapps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusBookingAppsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
