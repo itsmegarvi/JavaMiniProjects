9e7fba48c18f410d3fb674627da5070283039775
Garvit Shubham <mailme.garvit@gmail.com>
2023-03-21 19:25:44 +0530

    Forex-service initiated

---
+++ CurrencyConverter/forex-service/src/main/java/com/mis/forexservice/ForexServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mis.forexservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForexServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForexServiceApplication.class, args);
+	}
+
+}
---
+++ CurrencyConverter/forex-service/src/test/java/com/mis/forexservice/ForexServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mis.forexservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForexServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
