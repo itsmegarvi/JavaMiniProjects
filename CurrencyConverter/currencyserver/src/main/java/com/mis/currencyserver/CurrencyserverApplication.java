cb0462d286da91dde56689372cd8ef118a0cb6f0
Garvit Shubham <mailme.garvit@gmail.com>
2023-03-21 19:21:37 +0530

    Making a currency converter with microservices :server ready

--- CurrencyConverter/currencyserver/src/main/java/com/mis/currencyserver/CurrencyserverApplication.java
+++ CurrencyConverter/currencyserver/src/main/java/com/mis/currencyserver/CurrencyserverApplication.java
@@ -2,8 +2,10 @@ package com.mis.currencyserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class CurrencyserverApplication {
 
 	public static void main(String[] args) {
