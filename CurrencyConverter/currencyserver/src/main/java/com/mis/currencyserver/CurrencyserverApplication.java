67f153b622dccdd275ea3fa5f0a98da147c24cd7
Garvit Shubham <mailme.garvit@gmail.com>
2023-03-21 19:05:56 +0530

    Making a currency converter with microservices

---
+++ CurrencyConverter/currencyserver/src/main/java/com/mis/currencyserver/CurrencyserverApplication.java
@@ -0,0 +1,13 @@
+package com.mis.currencyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyserverApplication.class, args);
+	}
+
+}
---
+++ CurrencyConverter/currencyserver/src/test/java/com/mis/currencyserver/CurrencyserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mis.currencyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
